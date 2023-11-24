e68440f8192124433c3567a4d5b415bd53a30fc5
jeire07 <149458625+jeire07@users.noreply.github.com>
2023-11-24 10:49:37 +0900

    init

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public string solution(int n) {
+        string answer = "";
+        for(int i = 1; i <= n; i++)
+        {
+            answer += (i % 2) == 0 ? "박" : "수";
+        }
+        return answer;
+    }
+}
