79aa712060048be0bd120ca2d192a2bab4f77ade
jeire07 <149458625+jeire07@users.noreply.github.com>
2023-11-24 10:50:59 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.19 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cs
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cs
@@ -1,10 +1,7 @@
 public class Solution {
     public string solution(int n) {
         string answer = "";
-        for(int i = 1; i <= n; i++)
-        {
-            answer += (i % 2) == 0 ? "박" : "수";
-        }
+        for(int i = 1; i <= n; i++) answer += (i % 2) == 0 ? "박" : "수";
         return answer;
     }
 }
