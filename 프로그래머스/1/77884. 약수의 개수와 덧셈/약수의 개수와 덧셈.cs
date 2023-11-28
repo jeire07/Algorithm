ecf87692a5d3a846c9e5419d7338c68a520d5021
jeire07 <149458625+jeire07@users.noreply.github.com>
2023-11-28 09:39:05 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.20 ms, Memory: 31.7 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cs
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cs
@@ -3,34 +3,21 @@ using System;
 public class Solution {
     public int solution(int left, int right) {
         int answer = 0;
-        int index = 0;
         int length = right - left + 1;
-        int[] cnt = new int[length];
-        Array.Fill(cnt, 0);
         
         for(int i = left; i <= right; i++)
         {
+            int cnt = 0;
+            
             for(int j = 1; j * j <= i; j++)
             {
                 if(i % j == 0)
                 {
-                    if(j * j != i) cnt[index] += 2;
-                    else cnt[index]++;
+                    if(j * j != i) cnt += 2;
+                    else cnt++;
                 }
             }
-            index++;
-        }
-        
-        for(int i = 0; i < length; i++)
-        {
-            if(cnt[i] % 2 == 0)
-            {
-                answer += left + i;
-            }
-            else
-            {
-                answer -= left + i;
-            }
+            answer += ((cnt % 2) == 0) ? i : i * (-1);
         }
         return answer;
     }
