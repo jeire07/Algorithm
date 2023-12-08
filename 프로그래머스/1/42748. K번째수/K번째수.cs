06fb62262164ecbc48ae582af5c793e49914eb9e
jeire07 <149458625+jeire07@users.noreply.github.com>
2023-12-08 13:43:08 +0900

    [level 1] Title: K번째수, Time: 2.32 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] array, int[,] commands) {
+        int[] answer = new int[commands.GetLength(0)];
+
+        int[] tempArray = new int[array.Length];
+
+        for (int i = 0; i < commands.GetLength(0); i++)
+        {
+            Array.Copy(array, tempArray, array.Length);
+            Array.Sort(tempArray, commands[i, 0] - 1, commands[i, 1] - commands[i, 0] + 1);
+            tempArray = tempArray;
+            int index = commands[i, 0] + commands[i, 2] - 2;
+            answer[i] = tempArray[index];
+        }
+        return answer;
+    }
+}
