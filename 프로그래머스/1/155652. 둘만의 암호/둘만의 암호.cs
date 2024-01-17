51f360aa8f3595145d459b6f23667136b2c30eff
Wonjun Lee <149458625+jeire07@users.noreply.github.com>
2024-01-17 09:15:17 +0900

    [level 1] Title: 둘만의 암호, Time: 0.92 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.cs
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.cs
@@ -1,14 +1,31 @@
 using System;
 using System.Linq;
+using System.Text;
 
 public class Solution {
-    public string solution(string s, string skip, int index) {
-        string answer = "";
-        string a = new string("abcdefghijklmnopqrstuvwxyz".Where(x => !skip.Contains(x)).ToArray());
-        foreach(var t in s) {
-            answer += a[(a.IndexOf(t.ToString()) + index)%a.Length];
+    public string solution(string s, string skip, int index)
+    {
+        StringBuilder sb = new StringBuilder();
+        char[] chars = new char[26 - skip.Length];
+
+        for (int i = 0, j = 0; j <= 'z' - 'a'; j++)
+        {
+            char c = (char)((int)'a' + j);
+            if (!skip.Contains(c))
+            {
+                chars[i] = c;
+                i++;
+            }
         }
 
-        return answer;
+        for (int i = 0; i < s.Length; i++)
+        {
+            int newIndex = Array.IndexOf(chars, s[i]) + index;
+            int newAsciiIndex = newIndex >= chars.Length ? newIndex % chars.Length : newIndex;
+            char newAscii = chars[newAsciiIndex];
+
+            sb.Append(newAscii);
+        }
+        return sb.ToString();
     }
 }
